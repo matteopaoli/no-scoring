dba527e8f942ecc9904e71f72d3a68336922aa8a
Matteo Paoli <matteo_paoli@outlook.com>
2024-10-10 11:22:45 +0200

    improve layout generic product card

--- app/(lang)/app/GenericProductCard.tsx
+++ app/(lang)/app/GenericProductCard.tsx
@@ -15,7 +15,9 @@ const GenericProductCard = async () => {
 
   const product = await getProduct(user.genericProductId);
   const stripe = new Stripe(user.stripeSecretKey);
-  const paymentLinkUrl = (await stripe.paymentLinks.retrieve(product.paymentLinkId!)).url;
+  const paymentLinkUrl = (
+    await stripe.paymentLinks.retrieve(product.paymentLinkId!)
+  ).url;
 
   return (
     <Box
@@ -23,17 +25,19 @@ const GenericProductCard = async () => {
       bg="gray.100"
       borderRadius="md"
       boxShadow="md"
-      maxW="full"
-      mx="auto"
+      maxW="700px"
     >
       <Text fontSize="2xl" fontWeight="bold" mb={4}>
         Link di pagamento editabile
       </Text>
       <Text fontSize="lg" mb={4}>
-        Questo link permette di inserire qualsiasi cifra e può essere usato per uno o più prodotti contemporaneamente semplicemente inserendo la somma dei prodotti acquistati.
+        Questo link permette di inserire qualsiasi cifra e può essere usato per
+        uno o più prodotti contemporaneamente semplicemente inserendo la somma
+        dei prodotti acquistati.
       </Text>
       <Text fontSize="lg" mb={4}>
-        Consigliamo di stampare l&apos;etichetta con QR code e posizionarla vicino alla cassa per facilitarne l&apos;utilizzo.
+        Consigliamo di stampare l&apos;etichetta con QR code e posizionarla
+        vicino alla cassa per facilitarne l&apos;utilizzo.
       </Text>
       <Text fontSize="lg" fontWeight="bold">
         Link di Pagamento
@@ -44,54 +48,56 @@ const GenericProductCard = async () => {
       <Text fontSize="lg" fontWeight="bold">
         Immagini
       </Text>
-      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={6} mt={4}>
-        <Box textAlign="center">
-          <VStack spacing={2}>
-            <Image
-              src={product.qrcode!}
-              alt="QRCode"
-              height={200}
-              width={200}
-              objectFit="contain"
-            />
-            <Text fontWeight="bold">QR Code</Text>
-            <Button as="a" href={product.qrcode!} download="qrcode.png" colorScheme="brand">
-              Scarica QR Code
-            </Button>
-          </VStack>
-        </Box>
+      <SimpleGrid columns={{ base: 1, md: 3 }} mt={4} spacing={24} maxW="600px">
+        <VStack spacing={2} justifyContent="flex-end">
+          <Image
+            src={product.qrcode!}
+            alt="QRCode"
+            height={200}
+            width={200}
+            objectFit="contain"
+          />
+          <Button
+            as="a"
+            href={product.qrcode!}
+            download="qrcode.png"
+          >
+            <u>Scarica QR Code</u>
+          </Button>
+        </VStack>
+        <VStack spacing={2} justifyContent="flex-end">
+          <Image
+            src={user.genericProductSmallImage}
+            alt="Small Product"
+            height={200}
+            width={200}
+            objectFit="contain"
+          />
+          <Button
+            as="a"
+            href={user.genericProductSmallImage}
+            download="small_image.png"
+          >
+            <u>Scarica Immagine Piccola</u>
+          </Button>
+        </VStack>
 
-        <Box textAlign="center">
-          <VStack spacing={2}>
-            <Image
-              src={user.genericProductSmallImage}
-              alt="Small Product"
-              height={200}
-              width={200}
-              objectFit="contain"
-            />
-            <Text fontWeight="bold">Immagine Piccola</Text>
-            <Button as="a" href={user.genericProductSmallImage} download="small_image.png" colorScheme="brand">
-              Scarica Immagine Piccola
-            </Button>
-          </VStack>
-        </Box>
-
-        <Box textAlign="center">
-          <VStack spacing={2}>
-            <Image
-              src={user.genericProductLargeImage}
-              alt="Large Product"
-              height={200}
-              width={200}
-              objectFit="contain"
-            />
-            <Text fontWeight="bold">Immagine Grande</Text>
-            <Button as="a" href={user.genericProductLargeImage} download="large_image.png" colorScheme="brand">
-              Scarica Immagine Grande
-            </Button>
-          </VStack>
-        </Box>
+        <VStack spacing={2} justifyContent="flex-end">
+          <Image
+            src={user.genericProductLargeImage}
+            alt="Large Product"
+            height={200}
+            width={200}
+            objectFit="contain"
+          />
+          <Button
+            as="a"
+            href={user.genericProductLargeImage}
+            download="large_image.png"
+          >
+            <u>Scarica Immagine Grande</u>
+          </Button>
+        </VStack>
       </SimpleGrid>
     </Box>
   );
