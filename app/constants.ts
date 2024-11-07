5a6178b5814cdf3dd86e34ba2ffe2163d68b9b68
Matteo Paoli <matteo_paoli@outlook.com>
2024-11-07 23:32:33 +0700

    chore: bump version to 1.1.1

--- app/constants.ts
+++ app/constants.ts
@@ -22,4 +22,4 @@ export const provinces = [
     "Treviso", "Trieste", "Udine", "Varese", "Venezia", "Verbano-Cusio-Ossola",
     "Vercelli", "Verona", "Vibo Valentia", "Vicenza", "Viterbo", "Estero"
   ];
-export const VERSION="1.1.0"
+export const VERSION="1.1.1"
