d563334f1739b02f36ca6609594035ab5b7bd980
Matteo Paoli <matteo_paoli@outlook.com>
2024-11-07 13:35:49 +0700

    fix: wrong redirection home for subpartner

--- app/partner/(app)/page.tsx
+++ app/partner/(app)/page.tsx
@@ -3,11 +3,16 @@ import { redirect } from "next/navigation";
 import Statistics from "./Statistics";
 import { GridItem, SimpleGrid } from "@chakra-ui/react";
 import { auth } from "@/app/auth";
+import getUserFromAuth from "@/app/utils/getUserFromAuth";
 
 export default async function Page() {
   // const session = await auth()
   // const user = await getUser(session?.user?.email)
-  redirect('/partner/subpartners')
+  const user = await getUserFromAuth()
+  if (user.role === 'partner') {
+    redirect('/partner/subpartners')
+  }
+  else redirect('/partner/merchants')
   // const merchants = await getAllMerchants();
   // const sales = await getSales(user.id, user.role)
   // return (
