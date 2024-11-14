8196e3744bc2002bcfe45fae26890813903c780c
Matteo Paoli <matteo_paoli@outlook.com>
2024-11-14 21:33:38 +0700

    fix fee calculation logic

--- app/utils/fees.ts
+++ app/utils/fees.ts
@@ -1,6 +1,11 @@
 import { LEG_FEE_RATE, STRIPE_FEE_FIXED, STRIPE_FEE_VAR, VAT } from "../constants";
 
 export function getAmountWithFees(amount: number) {
-    const commission = (amount * (STRIPE_FEE_VAR + LEG_FEE_RATE) + STRIPE_FEE_FIXED) * VAT;
-    return amount + commission; // Add commission to price
+    const stripeFeeVarVAT = STRIPE_FEE_VAR * VAT;
+    const legFeeRateVAT = LEG_FEE_RATE * VAT;
+    const stripeFeeFixedVAT = STRIPE_FEE_FIXED * VAT;
+    
+    const totalFeeRateVAT = stripeFeeVarVAT + legFeeRateVAT;
+  
+    return (amount + stripeFeeFixedVAT) / (1 - totalFeeRateVAT);
 }
