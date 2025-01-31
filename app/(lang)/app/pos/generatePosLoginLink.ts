30fa92b0eedd8baf852e31be6aaea7f11cc2d853
Matteo Paoli <matteo_paoli@outlook.com>
2025-01-31 14:03:31 +0100

    fix: pos improvements

--- app/(lang)/app/pos/generatePosLoginLink.ts
+++ app/(lang)/app/pos/generatePosLoginLink.ts
@@ -4,7 +4,7 @@ import { UserService } from "@/app/services/userService";
 export default async function generatePosLoginLink(posId: string) {
     let pos = await UserService.getUserById(posId);
     if (!pos.id) throw new Error('POS not found');
-    await signIn('posMagicLinkCreation', { email: pos.email, redirect: false, redirectTo: '/pos' });
+    await signIn('posMagicLinkCreation', { email: pos.email, redirect: false, redirectTo: '/pos/view' });
     pos = await UserService.getUserById(posId);
     return pos.magicLinkUrl;
 }
